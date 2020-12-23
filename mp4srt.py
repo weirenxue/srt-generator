fa8b68b585959efbd7f8d599033cb176951ff86a
weirenxue <32257183+weirenxue@users.noreply.github.com>
2020-12-23 08:42:40 +0800

    Update mp4srt.py

--- mp4srt.py
+++ mp4srt.py
@@ -1,7 +1,7 @@
-from os import confstr
 import pathlib
 import datetime
 import moviepy.editor
+import platform
 
 mp4Path = 'mp4-file-path'
 fname = pathlib.Path(mp4Path)
@@ -14,19 +14,21 @@ def convert(seconds):
     return hours, mins, seconds
 def timestampToDate(timestamp):
     return str(datetime.datetime.fromtimestamp(timestamp))
+def timestampToUtcDate(timestamp):
+    return str(datetime.datetime.utcfromtimestamp(timestamp))
 
-utc8 = 8 * 60 * 60
 with open('result.srt', 'w', encoding="utf-8") as outfile:
     if fname.exists():
         video = moviepy.editor.VideoFileClip(mp4Path)
-        createTimestamp = fname.stat().st_birthtime
+        createTimestamp = int(fname.stat().st_ctime)
+        if platform.system() != 'Windows':
+            createTimestamp = int(fname.stat().st_birthtime)
         videoDuration = int(video.duration)
         for i in range(0, videoDuration):
-            thisRealTimestamp = fname.stat().st_birthtime + i
-            thisVideoTimestamp = thisRealTimestamp - createTimestamp - utc8
+            thisRealTimestamp = createTimestamp + i
+            thisVideoTimestamp = thisRealTimestamp - createTimestamp
             oneSecondLaterVideoTimestamp = thisVideoTimestamp + 1
             outfile.write(str(i + 1) + "\n")
-            outfile.write(timestampToDate(thisVideoTimestamp).split()[1] + ",000" + " --> " + timestampToDate(oneSecondLaterVideoTimestamp).split()[1] + ",000" + "\n")
+            outfile.write(timestampToUtcDate(thisVideoTimestamp).split()[1] + ",000" + " --> " + timestampToUtcDate(oneSecondLaterVideoTimestamp).split()[1] + ",000" + "\n")
             outfile.write(timestampToDate(thisRealTimestamp) + "\n")
             outfile.write("\n")
-
